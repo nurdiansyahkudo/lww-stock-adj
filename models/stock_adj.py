5f36f3150e33296b9fbc6e3287eb588b5584167f
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-14 17:37:30 +0700

    FIX: Models and Views

--- models/stock_adj.py
+++ models/stock_adj.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api, _
 class StockAdj(models.Model):
     _name = 'stock.adj'
     _description = 'Stock Adjustment'
-    _inherits = {'stock.quant': 'quant_id'}
+    _inherit = {'stock.quant': 'quant_id'}
 
     quant_id = fields.Many2one('stock.quant', required=True, ondelete='cascade')
 
