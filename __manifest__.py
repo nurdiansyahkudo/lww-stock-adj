7ee8ab157d143f7fd5170f8d1f184f848d5f4cc9
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-15 14:18:55 +0700

    UPDATE: whole module

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'depends': ['stock'],
     'data': [
-        'views/stock_adj_menu.xml',
+        # 'views/stock_adj_menu.xml',
         'views/stock_adj_views.xml',
     ],
     'installable': True,
