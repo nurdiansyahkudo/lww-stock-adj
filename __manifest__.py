7842f826554993bb0ecba9a00c723c3894de514f
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-15 13:42:02 +0700

    UPDATE: whole module

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,10 @@
 {
-    'name': 'Stock Adjustment Custom',
+    'name': 'Custom Stock Adjustment',
     'version': '1.0',
     'depends': ['stock'],
-    'category': 'Inventory',
-    'summary': 'Custom Stock Adjustment Model using stock.adj',
     'data': [
-        'views/stock_adj_views.xml',
-        # 'views/stock_adj_menu.xml',
+        'views/stock_adjustment_menu.xml',
     ],
     'installable': True,
-    'auto_install': False,
     'application': False,
 }
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import stock_adj
+# from . import stock_adj
