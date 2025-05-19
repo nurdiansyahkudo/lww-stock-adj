9e9a17dfe13ae57c5d90c69edcfb6b3a1e200eda
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-19 16:46:21 +0700

    FIX: models

--- models/stock_adj.py
+++ models/stock_adj.py
@@ -10,6 +10,7 @@ class StockAdj(models.Model):
     debit_line = fields.Monetary(string='Debit', compute='_compute_debit_credit_line', store=True)
     credit_line = fields.Monetary(string='Credit', compute='_compute_debit_credit_line', store=True)
     currency_id = fields.Many2one('res.currency', string='Currency', compute='_compute_currency_id', store=True)
+    is_adjustment = fields.Boolean(string="Is Adjustment")
 
     @api.depends('product_id')
     def _compute_currency_id(self):
@@ -51,7 +52,37 @@ class StockAdj(models.Model):
             'res_model': 'stock.quant',
             'type': 'ir.actions.act_window',
             'context': ctx,
-            'domain': [('location_id.usage', 'in', ['internal', 'transit'])],
+            'domain': [('location_id.usage', 'in', ['internal', 'transit']), ('is_adjustment', '=', True)],
+            'views': [(view_id, 'list')],
+            'help': """
+                <p class="o_view_nocontent_smiling_face">
+                    {}
+                </p><p>
+                    {} <span class="fa fa-long-arrow-right"/> {}</p>
+                """.format(_('Your stock is currently empty'),
+                           _('Press the CREATE button to define quantity for each product in your stock or import them from a spreadsheet throughout Favorites'),
+                           _('Import')),
+        }
+        return action
+    
+    @api.model
+    def action_view_inventory(self):
+        """ Similar to _get_quants_action except specific for inventory adjustments (i.e. inventory counts). """
+        self = self._set_view_context()
+        self._quant_tasks()
+
+        ctx = dict(self.env.context or {})
+        ctx['no_at_date'] = True
+        if self.env.user.has_group('stock.group_stock_user') and not self.env.user.has_group('stock.group_stock_manager'):
+            ctx['search_default_my_count'] = True
+        view_id = self.env.ref('stock.view_stock_quant_tree_inventory_editable').id
+        action = {
+            'name': _('Inventory Adjustments'),
+            'view_mode': 'list',
+            'res_model': 'stock.quant',
+            'type': 'ir.actions.act_window',
+            'context': ctx,
+            'domain': [('location_id.usage', 'in', ['internal', 'transit']), ('is_adjustment', '=', False)],
             'views': [(view_id, 'list')],
             'help': """
                 <p class="o_view_nocontent_smiling_face">
